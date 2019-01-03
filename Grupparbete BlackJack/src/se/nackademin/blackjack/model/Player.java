8ee9188bbd712eac4a2fae40373e1b8fe14e8000
MrEjonzzon <mrejonzzon@gmail.com>
2019-01-03 15:00:09 +0100

    restored player class

--- Grupparbete BlackJack/src/se/nackademin/blackjack/model/Player.java
+++ Grupparbete BlackJack/src/se/nackademin/blackjack/model/Player.java
@@ -1,5 +1,49 @@
 package se.nackademin.blackjack.model;
 
 public class Player {
+    private String name;
+    private int balance, handValue;
 
-}
+    public Player(String name, int balance) {
+	setName(name);
+	setBalance(balance);
+	String Hand[];
+	setHandValue(0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getHandValue() {
+        return handValue;
+    }
+
+    public void setHandValue(int handValue) {
+        this.handValue = handValue;
+    }
+
+    public void drawCard(int cardAmount) {
+	//deck.draw
+	//add to player.hand
+	//handValue += newcard
+
+
+    }
+
+    public int bet(int betAmount) {
+	int pot;
+	pot += betAmount;
+    }
