6698e6706ddeabe81690acceb1a011b231773e62
MrEjonzzon <mrejonzzon@gmail.com>
2019-01-03 14:32:40 +0100

    Added some menu code

--- Grupparbete BlackJack/src/se/nackademin/blackjack/Game.java
+++ Grupparbete BlackJack/src/se/nackademin/blackjack/Game.java
@@ -1,4 +1,5 @@
 package se.nackademin.blackjack;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,71 +10,86 @@ import se.nackademin.blackjack.service.Dealer;
 
 public class Game {
 
-	public static void main(String[] args) {
-		Dealer dealer = new Dealer();
-		List<Card> cards = dealer.createNewDeck();
-		System.out.println(cards);
-		System.out.println(dealer.shuffle(cards));
-		boolean loggedIn = false;
-		Scanner scanner = new Scanner(System.in);
-		int money, bet;
-		// Card card = new Card();
-		Database database = new Database();
-		// Player dealer = new Player();
-		// Player player = new Player();
+    public static void main(String[] args) {
+	boolean loggedIn = false;
+	boolean gameOn = true;
+	Scanner scanner = new Scanner(System.in);
+	int money, bet;
+	// Card card = new Card();
+	Database database = new Database();
+	// Player dealer = new Player();
+	// Player player = new Player();
+	Deck deck = new Deck();
 
-		//System.out.println(deck.toString());
+	System.out.println("Welcome to BlackJack");
+	System.out.println("[1] Login");
+	System.out.println("[2] Register");
+	System.out.print("Enter: ");
 
-		System.out.println("Welcome to BlackJack");
-		System.out.println("[1] Login");
-		System.out.println("[2] Register");
-		System.out.print("Enter: ");
-		
-		String selection = scanner.next();
-		try 
-		{
-			int select = Integer.parseInt(selection);
-			
-			switch (select) {
-			case 1:
-				System.out.print("Enter username: ");
-				String loggInName = scanner.next();
-				System.out.print("Enter password: ");
-				String loggInpassword = scanner.next();
-				database.login(loggInName, loggInpassword);
-				loggedIn = true;
-				break;
-				
-			case 2:
-				System.out.print("Enter username: ");
-				String registerName = scanner.next();
-				System.out.print("Enter password: ");
-				String registerPassword = scanner.next();
-				database.register(registerName, registerPassword);
-				break;
-				
-			default:
-				System.out.println("Invalid selection, try again");
-				break;
+	String selection = scanner.next();
+	try {
+	    int select = Integer.parseInt(selection);
 
-			}
+	    switch (select) {
+	    case 1:
+		System.out.print("Enter username: ");
+		String loggInName = scanner.next();
+		System.out.print("Enter password: ");
+		String loggInpassword = scanner.next();
+		database.login(loggInName, loggInpassword);
+		loggedIn = true;
+		break;
+
+	    case 2:
+		System.out.print("Enter username: ");
+		String registerName = scanner.next();
+		System.out.print("Enter password: ");
+		String registerPassword = scanner.next();
+		database.register(registerName, registerPassword);
+		break;
+
+	    default:
+		System.out.println("Invalid selection, try again");
+		break;
+
+	    }
+	} catch (NumberFormatException e) {
+	    System.out.println("Something went wrong! \nPlease enter numbers between 1-2");
+	}
+
+	while (loggedIn) {
+	    while (gameOn) {
+		// player.draw(2)
+		// dealer.draw(2)
+		// player.setHandvalue(card1 + card2)
+		// dealer.setHandvalue(card1 + card2)
+		System.out.println("Press 1 to Hit");
+		System.out.println("Press 2 to Stand");
+		String gameSelection = scanner.next();
+		try {
+		    int gameSelect = Integer.parseInt(gameSelection);
+		    switch (gameSelect) {
+		    case 1:
+			// player.draw(1)
+
+		    }
+
 		}
-		catch(NumberFormatException e)
-		{
-			System.out.println("Something went wrong! \nPlease enter numbers between 1-2");
+
+		catch (NumberFormatException e) {
+		    System.out.println("Something went wrong! \nPlease enter numbers between 1-2");
 		}
+		// deck.shuffle()
 
+		// syso "dealer has card : + card1.value"
+		// syso "press 1 to hit, 2 to stand"
 
-		while (loggedIn) {
-			// deck.shuffle()
-			// player.draw(2)
-			// dealer.draw(2)
-			// syso "dealer has card : + card1.value"
-			// syso "press 1 to hit, 2 to stand"
-
-		}
-
-		scanner.close();
+	    }
 
 	}
+
+	scanner.close();
+
+    }
+
 }
