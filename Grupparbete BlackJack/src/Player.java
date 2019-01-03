94552fb9a77abea27d1da8ddba7fe8404675c757
MrEjonzzon <mrejonzzon@gmail.com>
2019-01-03 13:12:58 +0100

    Added player Class with some code

--- Grupparbete BlackJack/src/Player.java
+++ Grupparbete BlackJack/src/Player.java
@@ -1,4 +1,61 @@
-
 public class Player {
+    private String name;
+    private int balance, handValue;
+    
+    public Player(String name, int balance) {
+	setName(name);
+	setBalance(balance);
+	String Hand[];
+	setHandValue(//sumofhand);
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getHandValue() {
+        return handValue;
+    }
+
+    public void setHandValue(int handValue) {
+        this.handValue = handValue;
+    }
+    
+    public void drawCard(int cardAmount) {
+	//deck.draw
+	//add to player.hand
+	//handValue += newcard
+
+	
+    }
+    
+    public int bet(int betAmount) {
+	int pot;
+	pot += betAmount;
+    }
+
+    public void playerDecision(int selection) {
+	switch(selection) {
+	case 1:
+	    drawCard(0);
+	case 2:
+	    
+	case 3:
+	case 4: 
+	}
+    }
+    
+    
 }
