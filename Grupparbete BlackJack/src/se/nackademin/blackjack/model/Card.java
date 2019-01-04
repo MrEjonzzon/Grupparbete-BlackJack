9761434f1f3cf53527ceca72077475af7fa28836
MrEjonzzon <mrejonzzon@gmail.com>
2019-01-04 10:59:39 +0100

    new card class with enum

--- Grupparbete BlackJack/src/se/nackademin/blackjack/model/Card.java
+++ Grupparbete BlackJack/src/se/nackademin/blackjack/model/Card.java
@@ -1,53 +1,44 @@
 package se.nackademin.blackjack.model;
-public class Card{
-	private String rank;
-	private String suit;
-	private int value;
-	
-	//Constructor of Card class
-	public Card (String rank, String suit, int value ) {
-		this.rank = rank;
-		this.suit = suit;
-		this.value = value;
-	}
-	
-	public String getRank() {
-		return rank;
-	}
-	public void setRank(String rank) {
-		this.rank = rank;
-	}
-	public String getSuit() {
-		return suit;
-	}
-	public void setSuit(String suit) {
-		this.suit = suit;
-	}
-	public int getValue() {
-		return value;
-	}
-	public void setValue(int value) {
-		this.value = value;
-	}
 
-	private char getSuiteChar() {
-		if (suit.equals("Clubs")) {
-			return '\u2663';
-		} else if (suit.equals("Diamonds")) {
-			return '\u2666';
-		} else if (suit.equals("Hearts")) {
-			return '\u2665';
-		} else if (suit.equals("Spades")) {
-			return '\u2660';
-		}
-		return ' ';
-	}
+import se.nackademin.blackjack.model.Deck.Rank;
+import se.nackademin.blackjack.model.Deck.Suit;
 
-	@Override
-	public String toString() {
-		return rank + " of " + getSuiteChar();
-	}
-}
+public class Card {
 
-
+    Rank rank;
+    private Suit suit;
     
+    public Card(Rank r, Suit s) {
+	this.rank = r;
+	this.suit = s;
+
+    }
+
+    public Rank getRank() {
+	return rank;
+    }
+
+    public void setRank(Rank rank) {
+	this.rank = rank;
+    }
+
+    public Suit getSuit() {
+	return suit;
+    }
+
+    public void setSuit(Suit suit) {
+	this.suit = suit;
+    }
+
+
+
+    public String toString() {
+	Suit suitString = this.suit;
+	Rank rankString = this.rank;
+	String cardString = "[" + rankString + " of " + suitString + "]";
+	return cardString;
+
+    }
+
+
+}
