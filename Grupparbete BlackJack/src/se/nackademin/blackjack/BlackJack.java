f294d27b638a69c67a163d8805d197e353965bfb
Amir Dolkhani <amirdolkhani@gmail.com>
2019-01-06 21:33:55 +0100

    Update balance on database after each round of play

--- Grupparbete BlackJack/src/se/nackademin/blackjack/BlackJack.java
+++ Grupparbete BlackJack/src/se/nackademin/blackjack/BlackJack.java
@@ -1,112 +1,83 @@
 package se.nackademin.blackjack;
 
 import se.nackademin.blackjack.db.Database;
-
 import se.nackademin.blackjack.model.Gambler;
-
 import se.nackademin.blackjack.service.BlackJackTable;
-
 import java.util.Scanner;
 
 public class BlackJack {
 
-    public static void main(String[] args) {
-
-	boolean loggedIn = false;
-
-	Scanner scanner = new Scanner(System.in);
-
-	Database database = new Database();
-
-	BlackJackTable table = new BlackJackTable(scanner);
-
-	while (loggedIn == false) {
-	    System.out.println("Welcome to Black Jack!");
-
-	    System.out.print("[1] Login\n[2] Register\nEnter: ");
-
-	    String loginMeny = scanner.next();
-
-	    try {
-
-		int userChoice = Integer.parseInt(loginMeny);
-
-		switch (userChoice) {
-
-		case 1:
-
-		    database.login();
-
-		    if (database.isGoToGame() == true) {
-
-			loggedIn = true;
-
-		    }
-
-		    break;
-		case 2:
-		    database.register();
-		    if (database.isGoToGame() == false) {
-			database.login();
-		    }
-
-		    break;
-		default:
-
-		    break;
-		}
-	    } catch (Exception e) {
-
-		System.out.println("Enter only numbers");
-	    }
-	    while (loggedIn) {
-		Gambler gambler = new Gambler(database.getUserName(), database.getBalance());
-		System.out.print("[1] Play\n[2] Check Balance\n[3] Deposit \n[4] Logout ");
-		String startMenu = scanner.next();
-		boolean gameSwitch = table.canBeginNewRound();
-		try {
-		    int userChoice = Integer.parseInt(startMenu);
-		    switch (userChoice) {
-		    case 1:
-			while (gameSwitch && gambler.getBalance() > 0) {
-			    System.out.println("How much would you like to bet?");
-			    int bet = scanner.nextInt();
-			    if (bet >= 0) {
-
-				table.startRound(gambler);
-				table.showHands(gambler);
-				table.play(gambler);
-				table.finishRound(gambler);
-				table.updateBalance(bet, gambler);
-				gameSwitch = false;
-			    } else {
-				System.out.println("Your balance is lower than bet");
-			    }
+	public static void main(String[] args) {
+		boolean loggedIn = false;
+		Scanner scanner = new Scanner(System.in);
+		Database database = new Database(scanner);
+		BlackJackTable table = new BlackJackTable(scanner, database);
+		while (loggedIn == false) {
+			System.out.println("Welcome to Black Jack!");
+			System.out.print("[1] Login\n[2] Register\nEnter: ");
+			String loginMeny = scanner.next();
+			try {
+				int userChoice = Integer.parseInt(loginMeny);
+				switch (userChoice) {
+				case 1:
+					database.login();
+					if (database.isGoToGame() == true) {
+						loggedIn = true;
+					}
+					break;
+				case 2:
+					database.register();
+					if (database.isGoToGame() == false) {
+						database.login();
+					}
+					break;
+				default:
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("Enter only numbers");
 			}
-		    case 2:
-			System.out.println("Your balance is:" + gambler.getBalance());
-			break;
-
-		    case 3:
-			System.out.println("How much do you want to deposit? ");
-			int deposit = scanner.nextInt();
-			gambler.setBalance(gambler.getBalance() + deposit);
-			break;
-		    case 4:
-			loggedIn = false;
-
-		    default:
-			break;
-
-		    }
-		} catch (Exception e) {
-		    System.out.println("Invalid Selection");
+			while (loggedIn) {
+				Gambler gambler = new Gambler(database.getUserName(), database.getBalance());
+				System.out.print("[1] Play\n[2] Check Balance\n[3] Deposit \n[4] Logout ");
+				String startMenu = scanner.next();
+				boolean gameSwitch = table.canBeginNewRound();
+				try {
+					int userChoice = Integer.parseInt(startMenu);
+					switch (userChoice) {
+					case 1:
+						while (gameSwitch && gambler.getBalance() > 0) {
+							System.out.println("How much would you like to bet?");
+							int bet = scanner.nextInt();
+							if (bet >= 0) {
+								table.startRound(gambler);
+								table.showHands(gambler);
+								table.play(gambler);
+								table.finishRound(gambler);
+								table.updateBalance(bet, gambler);
+								gameSwitch = false;
+							} else {
+								System.out.println("Your balance is lower than bet");
+							}
+						}
+					case 2:
+						System.out.println("Your balance is:" + gambler.getBalance());
+						break;
+					case 3:
+						System.out.println("How much do you want to deposit? ");
+						int deposit = scanner.nextInt();
+						gambler.setBalance(gambler.getBalance() + deposit);
+						break;
+					case 4:
+						loggedIn = false;
+					default:
+						break;
+					}
+				} catch (Exception e) {
+					System.out.println("Invalid Selection");
+				}
+			}
 		}
-
-	    }
+		scanner.close();
 	}
-
-	scanner.close();
-    }
-
 }
--- Grupparbete BlackJack/src/se/nackademin/blackjack/service/BlackJackTable.java
+++ Grupparbete BlackJack/src/se/nackademin/blackjack/service/BlackJackTable.java
@@ -1,137 +1,141 @@
 package se.nackademin.blackjack.service;
 
+import se.nackademin.blackjack.db.Database;
 import se.nackademin.blackjack.model.Deck;
 import se.nackademin.blackjack.model.Gambler;
 
 import java.util.Scanner;
 
 public class BlackJackTable {
-    private final String BANNER_START = "----------------------------------------------";
-    private final String BANNER_END = "----------------------------------------------";
+	private final String BANNER_START = "----------------------------------------------";
+	private final String BANNER_END = "----------------------------------------------";
 
-    private final Scanner scanner;
-    private final Deck deck;
-    private final Dealer dealer;
-    boolean gamblerWon = false;
+	private final Scanner scanner;
+	private final Deck deck;
+	private final Dealer dealer;
+	private final Database database;
+	boolean gamblerWon = false;
 
-    public BlackJackTable(Scanner scanner) {
-	this.scanner = scanner;
-	dealer = new Dealer();
-	deck = dealer.createNewDeck();
-	dealer.shuffle(deck);
-    }
-
-    public boolean canBeginNewRound() {
-	return deck.getCards().size() > 4;
-	
-    }
-
-    public void startRound(Gambler gambler) {
-	if (!canBeginNewRound()) {
-	    throw new RuntimeException("No more round available");
+	public BlackJackTable(Scanner scanner, Database database) {
+		this.scanner = scanner;
+		this.database = database;
+		dealer = new Dealer();
+		deck = dealer.createNewDeck();
+		dealer.shuffle(deck);
 	}
-	System.out.println(BANNER_START);
-	System.out.println("New Round");
-	System.out.println(" Player 1: " + gambler.getName());
-	System.out.println(" Player 2: Dealer");
-	System.out.println(BANNER_END);
-	dealer.hit(deck, dealer);
-	dealer.hit(deck, dealer);
-	dealer.hit(deck, gambler);
-	dealer.hit(deck, gambler);
-    }
 
-    public void play(Gambler gambler) {
-	int lastChoice = 0;
-	while (gamblerCanPlay(gambler) && lastChoice != 1) {
-	    lastChoice = getChoice();
-	    if (lastChoice == 2) {
+	public boolean canBeginNewRound() {
+		return deck.getCards().size() > 4;
+
+	}
+
+	public void startRound(Gambler gambler) {
+		if (!canBeginNewRound()) {
+			throw new RuntimeException("No more round available");
+		}
+		System.out.println(BANNER_START);
+		System.out.println("New Round");
+		System.out.println(" Player 1: " + gambler.getName());
+		System.out.println(" Player 2: Dealer");
+		System.out.println(BANNER_END);
+		dealer.hit(deck, dealer);
+		dealer.hit(deck, dealer);
 		dealer.hit(deck, gambler);
-		showHands(gambler);
-	    }
+		dealer.hit(deck, gambler);
 	}
-    }
 
-    private boolean gamblerCanPlay(Gambler gambler) {
-	if (gambler.hasBusted()) {
-	    return false;
+	public void play(Gambler gambler) {
+		int lastChoice = 0;
+		while (gamblerCanPlay(gambler) && lastChoice != 1) {
+			lastChoice = getChoice();
+			if (lastChoice == 2) {
+				dealer.hit(deck, gambler);
+				showHands(gambler);
+			}
+		}
 	}
-	return !gambler.isBlackJackHand();
-    }
 
-    private int getChoice() {
-	int choice = 0;
-	presentOptions();
-	while (choice != 1 && choice != 2) {
-	    choice = scanner.nextInt();
-	    if (choice != 1 && choice != 2) {
-		System.out.println("Invalid choice");
+	private boolean gamblerCanPlay(Gambler gambler) {
+		if (gambler.hasBusted()) {
+			return false;
+		}
+		return !gambler.isBlackJackHand();
+	}
+
+	private int getChoice() {
+		int choice = 0;
 		presentOptions();
-	    }
+		while (choice != 1 && choice != 2) {
+			choice = scanner.nextInt();
+			if (choice != 1 && choice != 2) {
+				System.out.println("Invalid choice");
+				presentOptions();
+			}
+		}
+		return choice;
 	}
-	return choice;
-    }
 
-    private void presentOptions() {
-	System.out.println("---- How do you want to continue? ----");
-	System.out.println("1) Stand");
-	System.out.println("2) Hit");
-    }
-
-    public void finishRound(Gambler gambler) {
-	while (dealer.getCurrentPoints() < 16) {
-	    dealer.hit(deck, dealer);
+	private void presentOptions() {
+		System.out.println("---- How do you want to continue? ----");
+		System.out.println("1) Stand");
+		System.out.println("2) Hit");
 	}
-	int gamblersPoints = gambler.getCurrentPoints();
-	int dealersPoints = dealer.getCurrentPoints();
-	System.out.println(BANNER_START);
-	System.out.println(" Finished Round ");
-	System.out.println(" " + gambler.getName() + "'s score: " + gamblersPoints);
-	System.out.println(" " + gambler.getName() + "'s hand: " + gambler.revealHand());
-	System.out.println(" Dealer's score: " + dealersPoints);
-	System.out.println(" Dealer's hand: " + dealer.revealHand());
-	String result = "";
-	if (gambler.hasBusted()) {
-	    result = gambler.getName() + " has busted and lost. Score exceeds 21.";
-	} else if (gambler.isBlackJackHand()) {
-	    result = gambler.getName() + " has blackjack hand. Gambler wins!";
-	    gamblerWon = true;
-	} else if (gamblersPoints == dealersPoints) {
-	    result = "Dealer has same score as " + gambler.getName() + ". " + gambler.getName() + " wins!";
-	    gamblerWon = true;
-	} else if (dealer.hasBusted()) {
-	    result = "Dealer has busted and lost. Score exceeds 21. " + gambler.getName() + " wins!";
-	    gamblerWon = true;
-	} else if (gamblersPoints > dealersPoints) {
-	    result = gambler.getName() + " wins, closer to 21!";
-	    gamblerWon = true;
-	} else if (gamblersPoints < dealersPoints) {
-	    result = "Dealer wins, closer to 21!";
-	}
-	System.out.println(" Result: " + result);
-	System.out.println(BANNER_END);
-	dealer.clearHand();
-	gambler.clearHand();
-    }
 
-    public void updateBalance(int bet, Gambler gambler) {
-	if (gamblerWon == true) {
-	    gambler.setBalance(gambler.getBalance() + bet);
-	} else {
-	    gambler.setBalance(gambler.getBalance() - bet);
+	public void finishRound(Gambler gambler) {
+		while (dealer.getCurrentPoints() < 16) {
+			dealer.hit(deck, dealer);
+		}
+		int gamblersPoints = gambler.getCurrentPoints();
+		int dealersPoints = dealer.getCurrentPoints();
+		System.out.println(BANNER_START);
+		System.out.println(" Finished Round ");
+		System.out.println(" " + gambler.getName() + "'s score: " + gamblersPoints);
+		System.out.println(" " + gambler.getName() + "'s hand: " + gambler.revealHand());
+		System.out.println(" Dealer's score: " + dealersPoints);
+		System.out.println(" Dealer's hand: " + dealer.revealHand());
+		String result = "";
+		if (gambler.hasBusted()) {
+			result = gambler.getName() + " has busted and lost. Score exceeds 21.";
+		} else if (gambler.isBlackJackHand()) {
+			result = gambler.getName() + " has blackjack hand. Gambler wins!";
+			gamblerWon = true;
+		} else if (gamblersPoints == dealersPoints) {
+			result = "Dealer has same score as " + gambler.getName() + ". " + gambler.getName() + " wins!";
+			gamblerWon = true;
+		} else if (dealer.hasBusted()) {
+			result = "Dealer has busted and lost. Score exceeds 21. " + gambler.getName() + " wins!";
+			gamblerWon = true;
+		} else if (gamblersPoints > dealersPoints) {
+			result = gambler.getName() + " wins, closer to 21!";
+			gamblerWon = true;
+		} else if (gamblersPoints < dealersPoints) {
+			result = "Dealer wins, closer to 21!";
+		}
+		System.out.println(" Result: " + result);
+		System.out.println(BANNER_END);
+		dealer.clearHand();
+		gambler.clearHand();
 	}
-    }
 
-    /**
-     * Show gamblers hand as well as dealers first card
-     * 
-     * @param gambler Gambler
-     */
-    public void showHands(Gambler gambler) {
-	System.out.println("Dealer Hand:");
-	dealer.showHand();
-	System.out.println();
-	System.out.println(gambler.getName() + "'s Hand:");
-	gambler.showHand();
-    }
+	public void updateBalance(int bet, Gambler gambler) {
+		if (gamblerWon == true) {
+			gambler.setBalance(gambler.getBalance() + bet);
+		} else {
+			gambler.setBalance(gambler.getBalance() - bet);
+		}
+		database.updateBalanceDB(gambler.getBalance());
+	}
+
+	/**
+	 * Show gamblers hand as well as dealers first card
+	 * 
+	 * @param gambler Gambler
+	 */
+	public void showHands(Gambler gambler) {
+		System.out.println("Dealer Hand:");
+		dealer.showHand();
+		System.out.println();
+		System.out.println(gambler.getName() + "'s Hand:");
+		gambler.showHand();
+	}
 }
